fbe8eef8d7d8d9cd2ecc8c850ba753065243d56d
Yogesh <yogeshthakur938920@gmail.com>
2025-08-23 20:27:50 +0530

    second commit

---
+++ app/api/inngest/route.js
@@ -0,0 +1,9 @@
+import {serve} from "inngest/next"
+import {inngest, syncUserCreation, syncUserDeletion, syncUserUpdation} from '@/config/inngest'
+
+export const {GET,POST,PUT} =serve({
+    client:inngest,
+    functions: [
+ syncUserCreation, syncUserUpdation, syncUserDeletion
+    ],
+})
--- config/inngest.js
+++ config/inngest.js
@@ -1,4 +1,6 @@
 import { Inngest } from "inngest";
+import connectDB from "./db";
+import User from "@/models/User";
 
 export const inngest = new Inngest({id:'quickcart-next'})
 
@@ -10,5 +12,51 @@ export const syncUserCreation = inngest.createFunction(
     },
     {
         event:"clerk/user.created"
+    },
+    async({event})=>{
+        const{id,first_name,last_name,email_addresses ,image_url}=event.data
+        const userData={
+            _id:id,
+            email:email_addresses[0].email_address ,
+            name: first_name +""+ last_name,
+           imageurl: image_url
+
+        }
+        await connectDB()
+            await User.create(userData)
+        
+    }
+
+)
+export const syncUserUpdation = inngest.createFunction(
+    {
+        id: 'update-user-with-clerk'
+    },
+    {
+        event: "clerk/user.updated"
+    },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const userData = {
+            email: email_addresses[0].email_address,
+            name: first_name + " " + last_name,
+            imageurl: image_url
+        }
+        await connectDB()
+        await User.findByIdAndUpdate(id, userData)
+    }
+)
+
+export const syncUserDeletion = inngest.createFunction(
+    {
+        id: 'delete-user-with-clerk'
+    },
+    {
+        event: "clerk/user.deleted"
+    },
+    async ({ event }) => {
+        const { id } = event.data
+        await connectDB()
+        await User.findByIdAndDelete(id)
     }
 )

341a2cb71faba1fcb4de8bb39e6ba3ea8fbdbb78
Yogesh <yogeshthakur938920@gmail.com>
2025-08-23 21:55:26 +0530

    changes in routes.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,9 +1,21 @@
-import {serve} from "inngest/next"
-import {inngest, syncUserCreation, syncUserDeletion, syncUserUpdation} from '@/config/inngest'
+// import {serve} from "inngest/next"
+// import {inngest, syncUserCreation, syncUserDeletion, syncUserUpdation} from '@/config/inngest'
 
-export const {GET,POST,PUT} =serve({
-    client:inngest,
-    functions: [
- syncUserCreation, syncUserUpdation, syncUserDeletion
-    ],
-})
+// export const {GET,POST,PUT} =serve({
+//     client:inngest,
+//     functions: [
+//  syncUserCreation, syncUserUpdation, syncUserDeletion
+//     ],
+// })
+
+// app/api/inngest/route.js
+
+export const runtime = "nodejs";   // 👈 Force Vercel to use Node.js serverless runtime
+
+import { serve } from "inngest/next";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [syncUserCreation, syncUserUpdation, syncUserDeletion],
+});

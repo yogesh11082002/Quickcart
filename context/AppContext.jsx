c0216b0c7b7ed4d4652e98be875dffe7bbe7f222
Yogesh <yogeshthakur938920@gmail.com>
2025-08-27 10:56:36 +0530

    changes for seller dashboared

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -19,7 +19,7 @@ export const AppContextProvider = (props) => {
 
     const [products, setProducts] = useState([])
     const [userData, setUserData] = useState(false)
-    const [isSeller, setIsSeller] = useState(true)
+    const [isSeller, setIsSeller] = useState(false)
     const [cartItems, setCartItems] = useState({})
 
     const fetchProductData = async () => {
@@ -27,7 +27,15 @@ export const AppContextProvider = (props) => {
     }
 
     const fetchUserData = async () => {
+
+       try {
+         if(user.publicMetadata.role==='seller'){
+                setIsSeller(true);
+        }
         setUserData(userDummyData)
+       } catch (error) {
+        
+       }
     }
 
     const addToCart = async (itemId) => {
@@ -81,8 +89,11 @@ export const AppContextProvider = (props) => {
     }, [])
 
     useEffect(() => {
-        fetchUserData()
-    }, [])
+        if (user) {
+            fetchUserData()
+        }
+        
+    }, [user])
 
     const value = {
         currency, router,

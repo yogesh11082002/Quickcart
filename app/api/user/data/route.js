544258d86d04314697c0b72276c92e29d4ac7276
Yogesh <yogeshthakur938920@gmail.com>
2025-08-27 12:16:29 +0530

    changes in user route file

--- app/api/user/data/route.js
+++ app/api/user/data/route.js
@@ -1,6 +1,6 @@
 import connectDB from "@/config/db";
 import User from "@/models/User";
-import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 

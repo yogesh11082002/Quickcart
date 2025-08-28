580d638ec9e9a42d8dbb5fdffde31b63f1e963f3
Yogesh <yogeshthakur938920@gmail.com>
2025-08-28 13:50:12 +0530

    all good

--- app/seller/product-list/page.jsx
+++ app/seller/product-list/page.jsx
@@ -1,3 +1,113 @@
+// 'use client'
+// import React, { useEffect, useState } from "react";
+// import { assets, productsDummyData } from "@/assets/assets";
+// import Image from "next/image";
+// import { useAppContext } from "@/context/AppContext";
+// import Footer from "@/components/seller/Footer";
+// import Loading from "@/components/Loading";
+// import axios from "axios";
+// import { toast } from "react-hot-toast";
+// import { useAuth } from "@clerk/nextjs";
+// const ProductList = () => {
+
+//   const { router, user } = useAppContext()
+//   const { getToken } = useAuth();
+
+//   const [products, setProducts] = useState([])
+//   const [loading, setLoading] = useState(true)
+
+//   const fetchSellerProduct = async () => {
+//  try {
+    
+//     const token = await getToken();
+
+//     const { data } = await axios.get("/api/product/seller-list", {
+//       headers: { Authorization: `Bearer ${token}` },
+//     });
+
+//     // console.log("API Response:", data); // Only log the response
+//     console.log("Seller list response:", data);
+
+
+//     if (data.success) {
+//      setProducts(data.products);
+//      setLoading(false);
+//     } else {
+//       toast.error(data.message);
+//     }
+//   }catch (error) {
+//     console.error("Error fetching user data:", error);
+//     toast.error(error.message);
+//   }
+// };
+
+//   useEffect(() => {
+//     if (user) {
+//       fetchSellerProduct();
+//     }
+    
+//   }, [user])
+
+//   return (
+//     <div className="flex-1 min-h-screen flex flex-col justify-between">
+//       {loading ? <Loading /> : <div className="w-full md:p-10 p-4">
+//         <h2 className="pb-4 text-lg font-medium">All Product</h2>
+//         <div className="flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20">
+//           <table className=" table-fixed w-full overflow-hidden">
+//             <thead className="text-gray-900 text-sm text-left">
+//               <tr>
+//                 <th className="w-2/3 md:w-2/5 px-4 py-3 font-medium truncate">Product</th>
+//                 <th className="px-4 py-3 font-medium truncate max-sm:hidden">Category</th>
+//                 <th className="px-4 py-3 font-medium truncate">
+//                   Price
+//                 </th>
+//                 <th className="px-4 py-3 font-medium truncate max-sm:hidden">Action</th>
+//               </tr>
+//             </thead>
+//             <tbody className="text-sm text-gray-500">
+//               {products.map((product, index) => (
+//                 <tr key={index} className="border-t border-gray-500/20">
+//                   <td className="md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3 truncate">
+//                     <div className="bg-gray-500/10 rounded p-2">
+//                       <Image
+//                         src={product.image[0]}
+//                         alt="product Image"
+//                         className="w-16"
+//                         width={1280}
+//                         height={720}
+//                       />
+//                     </div>
+//                     <span className="truncate w-full">
+//                       {product.name}
+//                     </span>
+//                   </td>
+//                   <td className="px-4 py-3 max-sm:hidden">{product.category}</td>
+//                   <td className="px-4 py-3">${product.offerPrice}</td>
+//                   <td className="px-4 py-3 max-sm:hidden">
+//                     <button onClick={() => router.push(`/product/${product._id}`)} className="flex items-center gap-1 px-1.5 md:px-3.5 py-2 bg-orange-600 text-white rounded-md">
+//                       <span className="hidden md:block">Visit</span>
+//                       <Image
+//                         className="h-3.5"
+//                         src={assets.redirect_icon}
+//                         alt="redirect_icon"
+//                       />
+//                     </button>
+//                   </td>
+//                 </tr>
+//               ))}
+//             </tbody>
+//           </table>
+//         </div>
+//       </div>}
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default ProductList;
+
+
+
 "use client";
 import React, { useEffect, useState } from "react";
 import { assets } from "@/assets/assets";
@@ -158,20 +268,16 @@ const ProductList = () => {
                       {/* Visit */}
                       <button
                         onClick={() => router.push(`/product/${product._id}`)}
-                        className="flex items-center gap-2 px-3 py-2 bg-orange-600 text-white rounded-md hover:bg-orange-700 transition"
+                        className="flex items-center gap-1 px-2 py-2 bg-orange-600 text-white rounded-md"
                       >
-                        <span className="text-sm font-medium">Visit</span>
-                        <Image
-                          className="w-4 h-4"
-                          src={assets.redirect_icon}
-                          alt="redirect_icon"
-                        />
+                        <span className="hidden md:block">Visit</span>
+                        
                       </button>
 
                       {/* Edit */}
                       <button
                         onClick={() => handleEditClick(product)}
-                        className="px-3 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition text-sm font-medium"
+                        className="px-2 py-2 bg-blue-600 text-white rounded-md"
                       >
                         Edit
                       </button>
@@ -179,7 +285,7 @@ const ProductList = () => {
                       {/* Delete */}
                       <button
                         onClick={() => handleDelete(product._id)}
-                        className="px-3 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 transition text-sm font-medium"
+                        className="px-2 py-2 bg-red-600 text-white rounded-md"
                       >
                         Delete
                       </button>

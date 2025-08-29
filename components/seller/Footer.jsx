6c9539c17c83b41cd8328c7d3641b0e1451d73ab
Yogesh <yogeshthakur938920@gmail.com>
2025-08-29 09:11:33 +0530

    changes in product file

--- components/seller/Footer.jsx
+++ components/seller/Footer.jsx
@@ -9,17 +9,17 @@ const Footer = () => {
         <Image className="hidden md:block" src={assets.logo} alt="logo" />
         <div className="hidden md:block h-7 w-px bg-gray-500/60"></div>
         <p className="py-4 text-center text-xs md:text-sm text-gray-500">
-          Copyright 2025 © greatstack.dev All Right Reserved.
+          Copyright @YogeshThakur | All Rights Reserved 
         </p>
       </div>
       <div className="flex items-center gap-3">
-        <a href="#">
+        <a href="https://www.facebook.com/yogesh.sengar.589">
           <Image src={assets.facebook_icon} alt="facebook_icon" />
         </a>
-        <a href="#">
+        <a href="https://twitter.com/YogeshT12554000">
           <Image src={assets.twitter_icon} alt="twitter_icon" />
         </a>
-        <a href="#">
+        <a href="https://www.instagram.com/yogesh_thakur_1108">
           <Image src={assets.instagram_icon} alt="instagram_icon" />
         </a>
       </div>
